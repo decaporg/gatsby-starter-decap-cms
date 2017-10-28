6897e6a97c657c1ab364fceb020fc86ffc919adc
Austin Green <austinzgreen@gmail.com>
2017-10-27 20:52:40 -0400

    redirect user to cms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import Helmet from 'react-helmet';
 
 export default function Index({ data }) {
   const { edges: posts } = data.allMarkdownRemark;
