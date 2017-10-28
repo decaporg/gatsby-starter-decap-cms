3b333e0f3aefd085a037a297cef7f3761f6bcb26
Austin Green <austinzgreen@gmail.com>
2017-10-27 21:24:13 -0400

    add redirect script to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,51 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-export default function Index({ data }) {
-  const { edges: posts } = data.allMarkdownRemark;
-  return (
-    <section className="section">
-      <div className="container">
-        {posts.filter(post => post.node.frontmatter.templateKey === 'blog-post').map(({ node: post }) => {
-          return (
-            <div className="content" style={{ border: '1px solid #eaecee', padding: '2em 4em' }} key={post.id}>
-              <p>
-                <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
-                <span> &bull; </span>
-                <small>{post.frontmatter.date}</small>
-              </p>
-              <p>
-                {post.excerpt}
-                <br />
-                <br />
-                <Link className="button is-info is-small" to={post.frontmatter.path}>
-                  Keep Reading
-                </Link>
-              </p>
-            </div>
-          );
-        })}
-      </div>
-    </section>
-  );
+export default class IndexPage extends React.Component {
+  componentDidMount() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
+          });
+        }
+      });
+    }
+  }
+
+  render() {
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
+    return (
+      <section className="section">
+        <Helmet>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+        </Helmet>
+        <div className="container">
+          {posts.filter(post => post.node.frontmatter.templateKey === 'blog-post').map(({ node: post }) => {
+            return (
+              <div className="content" style={{ border: '1px solid #eaecee', padding: '2em 4em' }} key={post.id}>
+                <p>
+                  <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+                  <span> &bull; </span>
+                  <small>{post.frontmatter.date}</small>
+                </p>
+                <p>
+                  {post.excerpt}
+                  <br />
+                  <br />
+                  <Link className="button is-info is-small" to={post.frontmatter.path}>
+                    Keep Reading
+                  </Link>
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
 }
 
 export const pageQuery = graphql`
