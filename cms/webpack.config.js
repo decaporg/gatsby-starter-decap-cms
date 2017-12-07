8461fa59db5bee1039574d17682fd70514948e16
Benaiah Mischenko <benaiah@mischenko.com>
2017-12-06 22:28:54 -0800

    Update formatting script and format CMS JS

--- cms/webpack.config.js
+++ cms/webpack.config.js
@@ -1,11 +1,11 @@
 // This webpack config is used to compile the JS for the CMS
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   entry: './cms.js',
   output: {
     filename: 'cms.bundle.js',
-    path: path.resolve(__dirname, '../static/admin/')
+    path: path.resolve(__dirname, '../static/admin/'),
   },
   module: {
     rules: [
@@ -13,17 +13,17 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["babel-preset-env", "babel-preset-react"]
-          }
-        }
-      }
-    ]
+            presets: ['babel-preset-env', 'babel-preset-react'],
+          },
+        },
+      },
+    ],
   },
   resolve: {
     alias: {
-      "site": "../src/"
-    }
-  }
-};
+      site: '../src/',
+    },
+  },
+}
