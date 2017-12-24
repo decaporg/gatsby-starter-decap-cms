b551708b235039c5fbe2181b2105015d09f08b5a
Marco Biedermann <marco.biedermann@gmx.de>
2017-12-24 11:20:36 +0100

    fix Navbar import

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
+import Navbar from '../components/Navbar';
 import './all.sass';
 
 const TemplateWrapper = ({ children }) => (
