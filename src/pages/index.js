a211f605f43f5987fa60a2448ffc3bb04d69d84a
Shawn Erquhart <shawn@erquh.art>
2018-02-23 19:18:49 -0500

    remove css import, now handled by plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,14 @@
 import React from "react";
 import Link from "gatsby-link";
-import Script from "react-load-script";
 import graphql from "graphql";
 
 export default class IndexPage extends React.Component {
-  handleScriptLoad() {
-    if (typeof window !== `undefined` && window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", user => {
-        if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
-        }
-      });
-    }
-    window.netlifyIdentity.init();
-  }
-
   render() {
     const { data } = this.props;
     const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <section className="section">
-        <Script
-          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
-          onLoad={() => this.handleScriptLoad()}
-        />
         <div className="container">
           <div className="content">
             <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
