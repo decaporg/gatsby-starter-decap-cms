6a42d5b36a8ddf800e22070aa53e936c9e74a75c
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-04-03 00:52:11 +0900

    Make post link to component from element array

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -2,16 +2,23 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 
+const PostTagLinks = ({ posts }) => {
+  return (
+    <ul className="taglist">
+      {posts.map( post => (
+        <li key={post.node.fields.slug}>
+          <Link to={post.node.fields.slug}>
+            <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 class TagRoute extends React.Component {
   render() {
     const posts = this.props.data.allMarkdownRemark.edges
-    const postLinks = posts.map(post => (
-      <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
-        </Link>
-      </li>
-    ))
     const tag = this.props.pathContext.tag
     const title = this.props.data.site.siteMetadata.title
     const totalCount = this.props.data.allMarkdownRemark.totalCount
@@ -29,7 +36,7 @@ class TagRoute extends React.Component {
               style={{ marginBottom: '6rem' }}
             >
               <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
-              <ul className="taglist">{postLinks}</ul>
+              <PostTagLinks posts={posts} />
               <p>
                 <Link to="/tags/">Browse all tags</Link>
               </p>
