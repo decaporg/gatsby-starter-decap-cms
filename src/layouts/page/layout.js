77bec217f150694d6471c2da28637d6f90c8b517
Kevin Meinhardt <kmeinhardt@NB230.local>
2018-07-01 13:02:16 +0200

    refactor templates and layouts

---
+++ src/layouts/page/layout.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby';
+import { HTMLContent } from '../../components/Content'
+
+const PageLayout = ({ title, content }) => {
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2>
+              <HTMLContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+};
+
+PageLayout.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+}
+
+export default PageLayout;
