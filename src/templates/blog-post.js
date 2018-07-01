d2178935bdf4f7b3990409d0b80f1188648171a5
Kevin Meinhardt <kmeinhardt@NB230.local>
2018-07-01 11:45:08 +0200

    upgarde to gatsby v2

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
-import Link from 'gatsby-link'
+import { Link, graphql } from 'gatsby';
 import Content, { HTMLContent } from '../components/Content'
 
 export const BlogPostTemplate = ({
--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Link from 'gatsby-link'
+import { Link, graphql } from 'gatsby';
 
 class TagRoute extends React.Component {
   render() {
