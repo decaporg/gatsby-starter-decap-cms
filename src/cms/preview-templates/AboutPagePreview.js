794bee71c939aef66d4de095d923d2af12976cda
Shawn Erquhart <shawn@erquh.art>
2018-08-30 11:50:49 -0400

    migrate for latest plugin updates

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import AboutPageTemplate from '../../components/AboutPageTemplate'
+import { AboutPageTemplate } from '../../templates/about-page'
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
   <AboutPageTemplate
