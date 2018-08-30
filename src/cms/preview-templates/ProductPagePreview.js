ff98ccb083857c7568b7e9465de6d5e13a963f02
Shawn Erquhart <shawn@erquh.art>
2018-08-30 13:05:17 -0400

    Gatsby plugin updates (#107)

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import ProductPageTemplate from '../../components/ProductPageTemplate'
+import { ProductPageTemplate } from '../../templates/product-page'
 
 const ProductPagePreview = ({ entry, getAsset }) => {
   const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
