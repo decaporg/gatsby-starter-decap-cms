96f1565f56754af020c2829cd3d8d9172f22d6f3
sw-yx <swyx@swyx.io>
2018-09-25 16:41:42 -0400

    fixed blogpost tempalte

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,66 +7,66 @@ import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 import MDXRenderer from "gatsby-mdx/mdx-renderer";
 
-export const BlogPostTemplate = props => {
-  console.log("blog page", props);
-  return <div>{JSON.stringify(Object.keys(props))}</div>;
+// export const BlogPostTemplate = props => {
+//   console.log("blog page", props);
+//   return <div>{JSON.stringify(Object.keys(props))}</div>;
+// };
+export const BlogPostTemplate = ({
+  content,
+  contentComponent,
+  description,
+  tags,
+  title,
+  helmet
+}) => {
+  const PostContent = contentComponent || Content;
+
+  return (
+    <section className="section">
+      {helmet || ""}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <p>{description}</p>
+            <PostContent>{content}</PostContent>
+            {tags && tags.length ? (
+              <div style={{ marginTop: `4rem` }}>
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map(tag => (
+                    <li key={tag + `tag`}>
+                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
-// export const BlogPostTemplate = ({
-//   content,
-//   contentComponent,
-//   description,
-//   tags,
-//   title,
-//   helmet
-// }) => {
-//   const PostContent = contentComponent || Content;
 
-//   return (
-//     <section className="section">
-//       {helmet || ""}
-//       <div className="container content">
-//         <div className="columns">
-//           <div className="column is-10 is-offset-1">
-//             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-//               {title}
-//             </h1>
-//             <p>{description}</p>
-//             <PostContent content={content} />
-//             {tags && tags.length ? (
-//               <div style={{ marginTop: `4rem` }}>
-//                 <h4>Tags</h4>
-//                 <ul className="taglist">
-//                   {tags.map(tag => (
-//                     <li key={tag + `tag`}>
-//                       <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-//                     </li>
-//                   ))}
-//                 </ul>
-//               </div>
-//             ) : null}
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// BlogPostTemplate.propTypes = {
-//   content: PropTypes.node.isRequired,
-//   contentComponent: PropTypes.func,
-//   description: PropTypes.string,
-//   title: PropTypes.string,
-//   helmet: PropTypes.instanceOf(Helmet)
-// };
+BlogPostTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.instanceOf(Helmet)
+};
 
 const BlogPost = ({ data }) => {
   const { mdx: post } = data;
-
+  console.log({ data });
   return (
     <Layout>
       <BlogPostTemplate
-        content={post.html}
-        contentComponent={HTMLContent}
+        content={post.code.body}
+        contentComponent={MDXRenderer}
         description={post.frontmatter.description}
         helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
         tags={post.frontmatter.tags}
