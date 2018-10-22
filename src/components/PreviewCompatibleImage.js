66b6ffcdd467844c1e646381cf7c768efc3599e8
Floyd Noel <floyd@camasdigital.com>
2018-10-22 11:37:48 -0600

    better props for preview component

--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -4,36 +4,31 @@ import Img from 'gatsby-image'
 
 const PreviewCompatibleImage = ({ imageInfo }) => {
   const imageStyle = { borderRadius: '5px' }
-  const { alt, childImageSharp, image } = imageInfo || {}
+  const { alt = '', childImageSharp, image } = imageInfo
 
   if (!!image && !!image.childImageSharp) {
     return (
-      <Img
-        style={imageStyle}
-        fluid={image.childImageSharp.fluid}
-        alt={!!alt ? alt : ''}
-      />
+      <Img style={imageStyle} fluid={image.childImageSharp.fluid} alt={alt} />
     )
   }
 
   if (!!childImageSharp) {
-    return (
-      <Img
-        style={imageStyle}
-        fluid={childImageSharp.fluid}
-        alt={!!alt ? alt : ''}
-      />
-    )
+    return <Img style={imageStyle} fluid={childImageSharp.fluid} alt={alt} />
   }
 
   if (!!image && typeof image === 'string')
-    return <img style={imageStyle} src={image} alt={!!alt ? alt : ''} />
+    return <img style={imageStyle} src={image} alt={alt} />
 
   return null
 }
 
 PreviewCompatibleImage.propTypes = {
-  imageInfo: PropTypes.object,
+  imageInfo: PropTypes.shape({
+    alt: PropTypes.string,
+    childImageSharp: PropTypes.object,
+    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
+    style: PropTypes.object,
+  }).isRequired,
 }
 
 export default PreviewCompatibleImage
