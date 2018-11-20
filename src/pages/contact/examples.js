cb36199bfd745fcdb8e08ff58aa3c2fde783cbb5
Zoli Veres <zltnvrs@gmail.com>
2018-11-20 09:12:42 +0200

    Added reCAPTHA instructions on Troubleshooting secion
    This is mostly just pointer to imorente's form example repository. Added just for convenience

--- src/pages/contact/examples.js
+++ src/pages/contact/examples.js
@@ -21,6 +21,8 @@ export default class Index extends React.Component {
               <h2>Troubleshooting</h2>
               <h3>Forms stop working after upgrading to Gatsby v2</h3>
               <p>This can be caused by the offline-plugin. <a href="https://github.com/gatsbyjs/gatsby/issues/7997#issuecomment-419749232">Workaround</a> is to use <code>?no-cache=1</code> in the POST url to prevent the service worker from handling form submissions</p>
+              <h3>Adding reCAPTCHA</h3>
+              <p>If you are planning to add reCAPTCHA please go to <a href="https://github.com/imorente/gatsby-netlify-form-example">imorente/gatsby-netlify-form-example</a> for a working example.</p>
             </div>
           </div>
         </section>
