60ea1ceef9a17d8119ff125aec77ac096070f269
Zoli Veres <zltnvrs@gmail.com>
2018-11-23 10:17:21 +0200

    title and description is configurable from gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Gatsby + Netlify CMS Starter',
+    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
