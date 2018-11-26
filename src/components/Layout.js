700f483be3955b768ef7ef7155a161d3cad0dc04
Zoli Veres <zltnvrs@gmail.com>
2018-11-26 17:21:28 +0200

    Lighthouse SEO score improvements (#139)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from "gatsby"
 
 import Navbar from '../components/Navbar'
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
-    <Navbar />
-    <div>{children}</div>
-  </div>
+  <StaticQuery
+    query={graphql`
+      query HeadingQuery {
+          site {
+            siteMetadata {
+              title,
+              description,
+            }
+          }
+        }
+    `}
+    render={data => (
+      <div>
+        <Helmet>
+          <html lang="en" />
+          <title>{data.site.siteMetadata.title}</title>
+          <meta name="description" content={data.site.siteMetadata.description} />
+        </Helmet>
+        <Navbar />
+        <div>{children}</div>
+      </div>
+    )}
+  />
 )
 
 export default TemplateWrapper
