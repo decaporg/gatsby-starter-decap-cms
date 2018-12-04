5850527ec26dc2b2523127d4809d247b23d937de
Zoli Veres <zltnvrs@gmail.com>
2018-12-04 10:01:51 +0200

    Navbar mobile view improvements
    Hamburger menu is implemented using the Bulma documentation example, along with some small design adjustments.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,49 @@ import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
-const Navbar = () => (
-  <nav className="navbar is-transparent">
+const Navbar = class extends React.Component {
+
+  componentDidMount() {
+    // Get all "navbar-burger" elements
+   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Check if there are any navbar burgers
+   if ($navbarBurgers.length > 0) {
+ 
+     // Add a click event on each of them
+     $navbarBurgers.forEach( el => {
+       el.addEventListener('click', () => {
+ 
+         // Get the target from the "data-target" attribute
+         const target = el.dataset.target;
+         const $target = document.getElementById(target);
+ 
+         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+         el.classList.toggle('is-active');
+         $target.classList.toggle('is-active');
+ 
+       });
+     });
+   }
+ }
+ 
+ render() {
+   return (
+  
+  <nav className="navbar is-transparent" role="navigation" aria-label="main-navigation">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
           <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
         </Link>
+        {/* Hamburger menu */}
+        <div class="navbar-burger burger" data-target="navMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-      <div className="navbar-start">
+      <div id="navMenu" className="navbar-menu">
+      <div className="navbar-start has-text-centered">
         <Link className="navbar-item" to="/about">
           About
         </Link>
@@ -25,7 +59,7 @@ const Navbar = () => (
           Form Examples
         </Link>
       </div>
-      <div className="navbar-end">
+      <div className="navbar-end has-text-centered">
         <a
           className="navbar-item"
           href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
@@ -37,8 +71,10 @@ const Navbar = () => (
           </span>
         </a>
       </div>
+      </div>
     </div>
   </nav>
-)
+  )}
+}
 
 export default Navbar
