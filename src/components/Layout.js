1a685ebf5078c36dd2e29a11ed19e4f18840f5f9
Zoli Veres <zltnvrs@gmail.com>
2018-12-04 16:51:58 +0200

    Kaldi template improvements (#144)
    Images , favicons are from the kaldi hugo template

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -23,6 +23,18 @@ const TemplateWrapper = ({ children }) => (
           <html lang="en" />
           <title>{data.site.siteMetadata.title}</title>
           <meta name="description" content={data.site.siteMetadata.description} />
+          
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+	        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+	        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+	
+	        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
+	        <meta name="theme-color" content="#fff" />
+
+	        <meta property="og:type" content="business.business" />
+          <meta property="og:title" content={data.site.siteMetadata.title} />
+          <meta property="og:url" content="/" />
+          <meta property="og:image" content="/img/og-image.jpg" />
         </Helmet>
         <Navbar />
         <div>{children}</div>
