57ae525235ee7c478d963fb4b2661262c1f23c5f
Zoli Veres <zltnvrs@gmail.com>
2018-12-11 09:18:35 +0200

    Index page is now a template page
    the index.js which is the main landing page is transformed into a template, basically it has the same content and template structure as the product page

---
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { IndexPageTemplate } from '../../templates/index-page'
+
+const IndexPagePreview = ({ entry, getAsset }) => {
+  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
+  const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
+
+  const entryTestimonials = entry.getIn(['data', 'testimonials'])
+  const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
+
+  const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
+  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
+
+  return (
+    <IndexPageTemplate
+      image={entry.getIn(['data', 'image'])}
+      title={entry.getIn(['data', 'title'])}
+      heading={entry.getIn(['data', 'heading'])}
+      description={entry.getIn(['data', 'description'])}
+      intro={{ blurbs }}
+      main={{
+        heading: entry.getIn(['data', 'main', 'heading']),
+        description: entry.getIn(['data', 'main', 'description']),
+        image1: {
+          image: getAsset(entry.getIn(['data', 'main', 'image1', 'image'])),
+          alt: entry.getIn(['data', 'main', 'image1', 'alt']),
+        },
+        image2: {
+          image: getAsset(entry.getIn(['data', 'main', 'image2', 'image'])),
+          alt: entry.getIn(['data', 'main', 'image2', 'alt']),
+        }
+      }}
+      fullImage={entry.getIn(['data', 'full_image'])}
+      testimonials={testimonials}
+      pricing={{
+        heading: entry.getIn(['data', 'pricing', 'heading']),
+        description: entry.getIn(['data', 'pricing', 'description']),
+        plans: pricingPlans,
+      }}
+    />
+  )
+}
+
+IndexPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+}
+
+export default IndexPagePreview
