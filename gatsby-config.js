13d2fa028dacb5d53ec01567326a934d3ecefbcd
Zoli Veres <zltnvrs@gmail.com>
2019-01-01 22:28:20 +0200

    purge-css improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,13 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    'gatsby-plugin-purgecss', // must be after other CSS plugins
+    {
+      resolve:'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+      options: {
+        develop: true,            // Activates purging in npm run develop
+        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
+      },
+    }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
