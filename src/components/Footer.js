783ca30cb4b2f4415eb104a0aa83d5a5c547b38d
Zoli Veres <zltnvrs@gmail.com>
2019-01-01 21:06:08 +0200

    Moved social media icons to src/img, more footer social media improvements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
+
 import logo from '../img/logo.svg'
+import facebook from '../img/social/facebook.svg'
+import instagram from '../img/social/instagram.svg'
+import twitter from '../img/social/twitter.svg'
+import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -9,7 +14,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Test"
+            alt="Kaldi"
             style={{ width: '14em', height: '10em' }}
           />
         </div>
@@ -17,6 +22,7 @@ const Footer = class extends React.Component {
           <div className="container has-background-black has-text-white-ter">
               <div className="columns">
                 <div className="column is-4">
+                <p className="menu-label">KALDI</p>
                 <section className="menu">
                     <ul className="menu-list">
                       <li><Link to="/" className="navbar-item">Home</Link></li>
@@ -43,8 +49,7 @@ const Footer = class extends React.Component {
                 </div>
                 <div className="column is-4">
                 <section>
-                  
-                  <p class="menu-label">Find out more</p>
+                  <p className="menu-label">Find out more</p>
                   <ul className="menu-list">
                   <li><Link className="navbar-item" to="/contact">
                       Schedule and appointment
@@ -52,15 +57,44 @@ const Footer = class extends React.Component {
                   </ul>
                 </section>
                 </div>
-                <div className="column is-4">
-                <a title="facebook" href="https://facebook.com">
-                  <span className="icons fas fa-lg"></span>
+                <div className="column is-4 social">
+                <p className="menu-label">Social media</p>
+                  <a title="facebook" href="https://facebook.com">
+                  
+                    <img 
+                      src={facebook}
+                      alt="Facebook"
+                      style={{ width: '1em', height: '1em' }}
+                    />
+                  
+                  </a>
+                  <a title="twitter" href="https://twitter.com">
+                  
+                  <img
+                    className="fas fa-lg"
+                    src={twitter}
+                    alt="Twitter"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                  
+                </a>
+                <a title="instagram" href="https://instagram.com">
+                  <img
+                  src={instagram}
+                  alt="Instagram"
+                  style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="vimeo" href="https://vimeo.com">
+                  <img
+                  src={vimeo}
+                  alt="Vimeo"
+                  style={{ width: '1em', height: '1em' }}
+                  />
                 </a>
                 </div>
               </div>
-
             </div>
-          
         </div>
       </footer>
     )
