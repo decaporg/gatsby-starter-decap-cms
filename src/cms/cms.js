7c96b00e981a83b6ba9b3f2f2eeda5e5700a4c2a
meets <timellemeet@users.noreply.github.com>
2019-01-02 22:11:46 +0100

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,1 @@
 import CMS from 'netlify-cms'
-
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
