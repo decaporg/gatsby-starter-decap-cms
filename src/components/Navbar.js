5ee91b8e030dff562fd9e6e5f2aef83a19a2b897
Zoli Veres <zltnvrs@gmail.com>
2019-01-03 23:57:36 +0200

    Added blog link to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -54,6 +54,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/products">
                 Products
               </Link>
+              <Link className="navbar-item" to="/blog">
+                Blog
+              </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
