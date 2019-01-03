0106a000587d31c148898e23733fd15948c0782f
Zoli Veres <zltnvrs@gmail.com>
2019-01-03 22:21:53 +0200

    Landing Index: Fixed second sentence positioning

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -41,24 +41,32 @@ export const IndexPageTemplate = ({
                 <h1
                   className="has-text-weight-bold is-size-2"
                   style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
+                    boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+                    backgroundColor: 'rgb(255, 68, 0)',
                     color: 'white',
-                    padding: '1rem',
+                    padding: '0.75rem',
+                    position: 'absolute',
+                    top: '25%',
+                    left: '5%',
+                    lineHeight: '1'
                   }}
                 >
                   {title}
                 </h1>
-                <span className="has-text-weight-bold is-size-4"
+                <h3 className="has-text-weight-bold is-size-4"
                     style={{
-                      boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                      backgroundColor: '#f40',
+                      boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+                      backgroundColor: 'rgb(255, 68, 0)',
                       color: 'white',
-                      padding: '1rem',
+                      padding: '.75rem',
+                      position: 'absolute',
+                      top: '40%',
+                      left: '5%',
+                      lineHeight: '1'
                     }}
                 >
                   {subheading}
-                </span>
+                </h3>
               </div>
               <div className="content">
                 <div className="tile">
