44bdae1e83ee9773495862e0144d5a3f57de0e86
Zoli Veres <zltnvrs@gmail.com>
2019-01-03 22:21:28 +0200

    Blog: Index page has a nice background image

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -13,7 +13,24 @@ export default class BlogIndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+            <div
+                className="full-width-image-container margin-top-0"
+                style={{
+                  backgroundImage: `url('/img/blog-index.jpg')`,
+                }}
+              >
+                <h1
+                  className="has-text-weight-bold is-size-1"
+                  style={{
+                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+                    backgroundColor: '#f40',
+                    color: 'white',
+                    padding: '1rem',
+                  }}
+                >
+                  Latest Stories
+                </h1>
+              </div>
             </div>
             {posts
               .map(({ node: post }) => (
