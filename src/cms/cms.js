2e2aec6a00d25ea8dff7a0a39de0faec3050b4a5
Zoli Veres <zltnvrs@gmail.com>
2019-01-10 10:50:33 +0200

    Registered Preview template for Landing Page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import IndexPagePreview from './preview-templates/IndexPagePreview'
 
+CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
