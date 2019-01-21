9938df3af818c765700d1ca6fc3035a7a7c28d13
Zoli Veres <zltnvrs@gmail.com>
2019-01-21 10:05:50 +0200

    NetlifyCMS changes for the Index Page (init. version)

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -6,22 +6,21 @@ const IndexPagePreview = ({ entry, getAsset }) => {
   const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
   const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
 
-  const entryTestimonials = entry.getIn(['data', 'testimonials'])
-  const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
-
-  const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
-  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
-
   return (
     <IndexPageTemplate
       image={entry.getIn(['data', 'image'])}
       title={entry.getIn(['data', 'title'])}
       heading={entry.getIn(['data', 'heading'])}
+      subheading = {entry.getIn(['data', 'subheading'])}
       description={entry.getIn(['data', 'description'])}
       intro={{ blurbs }}
       main={{
         heading: entry.getIn(['data', 'main', 'heading']),
         description: entry.getIn(['data', 'main', 'description']),
+        mainpitch: {
+          title: entry.getIn(['data','mainpitch', 'title']),
+          description: entry.getIn(['data','mainpitch', 'description'])
+        },
         image1: {
           image: getAsset(entry.getIn(['data', 'main', 'image1', 'image'])),
           alt: entry.getIn(['data', 'main', 'image1', 'alt']),
