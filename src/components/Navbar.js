90d5832b6856395f7d0b5c6e5d9897e2492baf13
talves <tony@3alves.com>
2019-05-03 09:37:39 -0700

    fix: link to this repo.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -79,7 +79,7 @@ const Navbar = class extends React.Component {
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
                 target="_blank"
                 rel="noopener noreferrer"
               >
