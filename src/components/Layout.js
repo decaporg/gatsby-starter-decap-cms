ff38a8c3c4efc7ed23590eeabfbd733928a58f1b
Heather <heatherlaurendavidson@gmail.com>
2019-06-24 23:02:18 +0100

    Update React Helmet import (#244)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import './all.sass'
