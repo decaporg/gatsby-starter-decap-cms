80fd1dcd8b1273a45e7c7c82a2b3de0d98f8ec13
Erez Rokah <erezrokah@users.noreply.github.com>
2019-12-31 20:20:46 +0200

    fix(blog-template): convert immutable list to array (#454)

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -2,14 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { BlogPostTemplate } from '../../templates/blog-post'
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
-)
+const BlogPostPreview = ({ entry, widgetFor }) => {
+  const tags = entry.getIn(['data', 'tags'])
+  return (
+    <BlogPostTemplate
+      content={widgetFor('body')}
+      description={entry.getIn(['data', 'description'])}
+      tags={tags && tags.toJS()}
+      title={entry.getIn(['data', 'title'])}
+    />
+  )
+}
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
