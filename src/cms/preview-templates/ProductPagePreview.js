b31c895b266f8891799d7faf9364bc0fce227ac8
erezrokah <erezrokah@users.noreply.github.com>
2020-02-13 12:55:48 +0200

    fix: use getAsset in preview templates

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -14,7 +14,7 @@ const ProductPagePreview = ({ entry, getAsset }) => {
 
   return (
     <ProductPageTemplate
-      image={entry.getIn(['data', 'image'])}
+      image={getAsset(entry.getIn(['data', 'image']))}
       title={entry.getIn(['data', 'title'])}
       heading={entry.getIn(['data', 'heading'])}
       description={entry.getIn(['data', 'description'])}
