cd2c983676f0b254e80fcd7897f573cfc5d274cd
Usmaniq298 <75735544+Usmaniq298@users.noreply.github.com>
2020-12-10 13:38:53 +0500

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -25,7 +25,7 @@ export const BlogPostTemplate = ({
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
               {title}
             </h1>
-            <p>{description}</p>
+            <p>{My name is Usman S/o M Iqbal}</p>
             <PostContent content={content} />
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
