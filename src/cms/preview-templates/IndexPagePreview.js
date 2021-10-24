7a8f31e58dde77c6118e66e18ef6f479b835fa64
FreedomWriter <natalie.davis@netlify.com>
2021-10-24 12:59:14 -0500

    IMPROVE: resolved lint warnings not showing up in browser console

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { IndexPageTemplate } from '../../templates/index-page'
+import React from "react";
+import PropTypes from "prop-types";
+import IndexPage from "../../templates/index-page";
 
 const IndexPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
+  const data = entry.getIn(["data"]).toJS();
 
   if (data) {
     return (
-      <IndexPageTemplate
+      <IndexPage
         image={getAsset(data.image)}
         title={data.title}
         heading={data.heading}
@@ -16,17 +16,17 @@ const IndexPagePreview = ({ entry, getAsset }) => {
         intro={data.intro || { blurbs: [] }}
         mainpitch={data.mainpitch || {}}
       />
-    )
+    );
   } else {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
-}
+};
 
 IndexPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   getAsset: PropTypes.func,
-}
+};
 
-export default IndexPagePreview
+export default IndexPagePreview;
