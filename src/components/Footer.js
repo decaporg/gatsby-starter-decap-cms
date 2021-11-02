e51a5848d878001e896d10b9041b639b80a705dc
FreedomWriter <natalie.davis@netlify.com>
2021-11-02 13:53:12 -0500

    NEW: Footer uses hooks now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
 import vimeo from "../img/social/vimeo.svg";
 
-const Footer = class extends React.Component {
-  render() {
+const Footer = () => {
+  
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
@@ -109,7 +109,6 @@ const Footer = class extends React.Component {
         </div>
       </footer>
     );
-  }
 };
 
 export default Footer;
