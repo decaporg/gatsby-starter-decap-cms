dee98d606fc6ca4437d8d2757ac7a4d8758adcda
FreedomWriter <natalie.davis@netlify.com>
2021-11-02 13:45:50 -0500

    NEW: BlogRoll uses hooks now

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -3,65 +3,64 @@ import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
 
-class BlogRollTemplate extends React.Component {
-  render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
 
-    return (
-      <div className="columns is-multiline">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                          width:
-                            post.frontmatter.featuredimage.childImageSharp
-                              .gatsbyImageData.width,
-                          height:
-                            post.frontmatter.featuredimage.childImageSharp
-                              .gatsbyImageData.height,
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
+const BlogRollTemplate = (props) => {
+  
+  const { edges: posts } = props.data.allMarkdownRemark;
+
+  return (
+    <div className="columns is-multiline">
+      {posts &&
+        posts.map(({ node: post }) => (
+          <div className="is-parent column is-6" key={post.id}>
+            <article
+              className={`blog-list-item tile is-child box notification ${
+                post.frontmatter.featuredpost ? 'is-featured' : ''
+              }`}
+            >
+              <header>
+                {post?.frontmatter?.featuredimage && (
+                  <div className="featured-thumbnail">
+                    <PreviewCompatibleImage
+                      imageInfo={{
+                        image: post.frontmatter.featuredimage,
+                        alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                        width:
+                          post.frontmatter.featuredimage.childImageSharp
+                            .gatsbyImageData.width,
+                        height:
+                          post.frontmatter.featuredimage.childImageSharp
+                            .gatsbyImageData.height,
+                      }}
+                    />
+                  </div>
+                ) }
+                <p className="post-meta">
+                  <Link
+                    className="title has-text-primary is-size-4"
+                    to={post.fields.slug}
+                  >
+                    {post.frontmatter.title}
                   </Link>
+                  <span> &bull; </span>
+                  <span className="subtitle is-size-5 is-block">
+                    {post.frontmatter.date}
+                  </span>
                 </p>
-              </article>
-            </div>
-          ))}
-      </div>
-    )
-  }
+              </header>
+              <p>
+                {post.excerpt}
+                <br />
+                <br />
+                <Link className="button" to={post.fields.slug}>
+                  Keep Reading →
+                </Link>
+              </p>
+            </article>
+          </div>
+        ))}
+    </div>
+  )
 }
 
 BlogRoll.propTypes = {
