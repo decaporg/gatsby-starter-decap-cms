e488c1920324e09e113a19d9d69a146c1ecfb7a2
emanuele-em <100081325+emanuele-em@users.noreply.github.com>
2022-07-13 09:33:17 +0200

    query fix, "filter" missing

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -116,7 +116,7 @@ export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+    markdownRemark(filter: {frontmatter: {templateKey: {eq: "index-page"}}}) {
       frontmatter {
         title
         image {
